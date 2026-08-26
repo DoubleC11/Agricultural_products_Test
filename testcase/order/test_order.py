c9bb1f08b400a57ced2995124ef7c63fc8a0d642
DoubleC11 <3437731868@qq.com>
2026-08-26 10:03:00 +0800

    order测试用例

---
+++ testcase/order/test_order.py
@@ -0,0 +1,17 @@
+import allure
+import pytest
+
+from utils.handle_data.yaml_handler import read_yaml
+from utils.handle_data.configParse import ConfigParse
+
+
+conf = ConfigParse()
+
+
+@allure.feature('订单模块')
+class TestOrder:
+    @pytest.mark.order(-1)
+    @pytest.mark.parametrize("data", read_yaml("./data/order.yaml"))
+    def test_create_order(self, data,re_client):
+        re_client.excute_test_cases(data)
+
